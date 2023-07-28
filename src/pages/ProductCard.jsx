b14d95b68736f4487c7e91d3fd13afcda2ba2a11
ArpitaBeheragit <138752080+ArpitaBeheragit@users.noreply.github.com>
2023-07-28 17:50:17 +0530

    Update ProductCard.jsx
    please install react helmet syntax:- npm i react-helmet

--- src/pages/ProductCard.jsx
+++ src/pages/ProductCard.jsx
@@ -1,6 +1,7 @@
 import "./ProductCard.css";
 import { useState } from "react";
 // import sort from "../Images/Sort.png"
+import {Helmet, link} from "react-helmet";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import {
@@ -17,38 +18,69 @@ import {
   Heading
 } from "@chakra-ui/react";
 
-export const ProductCard = ({ id, image, price, name, category }) => {
+export const ProductCard = (props) => {
+  const { id, image, price, name, shipping, star, category } = props;
+  const [wishlist,setwishlist]=useState(false);
   const navigate = useNavigate();
   function nav() {
     navigate(`/products/${id}`);
   }
+  const addtowishlist =  async () => {
+    let dataobj={
+      id:id,
+      image:image,
+      price:price,
+      name:name,
+      shipping:shipping,
+      star:star,
+      category:category
+    }
+    let res = await fetch(`http://localhost:3/wishlist`, {
+        method: "POST",
+        body: JSON.stringify(dataobj),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    setwishlist(!wishlist);
+}
   return (
-    <Card maxW="sm" bgColor="transparent" id="Card">
+    
+    <Card maxW="sm" maxH="sm" bgColor="transparent" id="Card">
       <CardBody>
-        <Image src={image} alt={id} borderRadius="lg" />
-        <Stack mt="6" spacing="1">
+      <Helmet>
+          <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+        /></Helmet>
+         <button className="wishlist" onClick={()=>addtowishlist()} style={{color: wishlist? "red":"black"}}><i class="fa fa-heart"></i></button>
+        <Image src={image} alt={id} borderRadius="lg" ml="12" />
+       
+        <Stack spacing="1" mt="-2">
             <div className="head">
           <Heading className="text" size="lg">₹{price}</Heading>
+          
           <span>
-            <a className="details" size="md" href="#" onClick={nav}>Details</a>
+            <a class="details" size="md" href="#" target="_blank" onClick={nav}>Details</a>
           </span>
           </div>
           <Text color='#4a2a64' fontSize="lg" textAlign="left" fontFamily="MuliRegular, Helvetica Neue, Helvetica, Arial, sans-serif;">Check Delivery Date 
           </Text>
           <Text textAlign="left" fontFamily="InterRegular,Helvetica Neue,Helvetica,Arial,sans-serif;" color=" #9d9fa4;" size="lg" spacing="0">{name}</Text>
         </Stack>
-      
-        <ButtonGroup spacing="5">
+        <ButtonGroup spacing="5" mt="2">
           <Button borderRadius= "10px"
     border=" solid 1px #4f3267;"
-    fontFamily= "InterRegular,Helvetica Neue,Helvetica,Arial,sans-serif;" color=" #231535;" mt="2">
+    fontFamily= "InterRegular,Helvetica Neue,Helvetica,Arial,sans-serif;" color=" #231535;">
             Book Try at Home
           </Button>
-          <Button border= "solid 1px #75d167;"  fontFamily= "InterRegular,Helvetica Neue,Helvetica,Arial,sans-serif;" color=" #75d167;" mt="2" >
+          <Button border= "solid 1px #75d167;"  fontFamily= "InterRegular,Helvetica Neue,Helvetica,Arial,sans-serif;" color=" #75d167;" >
             Live Video Call
           </Button>
         </ButtonGroup>
         </CardBody>
+        
     </Card>
   );
 };
