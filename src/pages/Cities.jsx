43cc32eacd0f688e7fe24822a0dbcc6d404de84f
Loria <loriamgartia@gmail.com>
2023-07-28 22:01:28 +0530

    changed App.css and Cities.jsx files

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -3,56 +3,54 @@
 export const Cities=()=>{
     return(
         <div classNameName="city-main">
-            <div classNameName="mumbai">
-                <div classNameName="mumbai-photo">
-                </div>
-                <h4 classNameName="city-title">Mumbai</h4>
-                <div classNameName="city-margin">
-                </div>
-               <p classNameName="stores-number">18 Stores</p>
-            </div>
-            <div className="bangalore">
-                <div className="bangalore-photo">
+            <div className="city-div">
+                <div className="city-photo">
+                <img src={require('../images/bangalore.png')}/>
                 </div>
                 <h4 className="city-title">Bangalore</h4>
                 <div className="city-margin">
                 </div>
                <p className="stores-number">12 Stores</p>
             </div>
-            <div className="mumbai">
-                <div className="mumbai-photo">
+            <div className="city-div">
+                <div className="city-photo">
+                <img src={require('../images/ahemdabad.png')}/>
                 </div>
-                <h4 className="city-title">Mumbai</h4>
+                <h4 className="city-title">Ahemdabad</h4>
+                <div className="city-margin">
+                </div>
+               <p className="stores-number">10 Stores</p>
+            </div>
+            <div className="city-div">
+                <div className="city-photo">
+                <img src={require('../images/chennai.png')}/>
+                </div>
+                <h4 className="city-title">Chennai</h4>
+                <div className="city-margin">
+                </div>
+               <p className="stores-number">20 Stores</p>
+            </div>
+            <div className="city-div">
+                <div className="city-photo">
+                <img src={require('../images/delhi.png')}/>  
+                </div>
+                <h4 className="city-title">Delhi</h4>
+                <div className="city-margin">
+                </div>
+               <p className="stores-number">7 Stores</p>
+            </div>
+            <div className="city-div">
+                <div className="city-photo">
+                <img src={require('../images/kolkata.png')}/>
+                </div>
+                <h4 className="city-title">Kolkata</h4>
                 <div className="city-margin">
                 </div>
                <p className="stores-number">18 Stores</p>
             </div>
-            <div className="mumbai">
-                <div className="mumbai-photo">
-                </div>
-                <h4 className="city-title">Mumbai</h4>
-                <div className="city-margin">
-                </div>
-               <p className="stores-number">18 Stores</p>
-            </div>
-            <div className="mumbai">
-                <div className="mumbai-photo">
-                </div>
-                <h4 className="city-title">Mumbai</h4>
-                <div className="city-margin">
-                </div>
-               <p className="stores-number">18 Stores</p>
-            </div>
-            <div className="mumbai">
-                <div className="mumbai-photo">
-                </div>
-                <h4 className="city-title">Mumbai</h4>
-                <div className="city-margin">
-                </div>
-               <p className="stores-number">18 Stores</p>
-            </div>
-            <div className="mumbai">
-                <div className="mumbai-photo">
+            <div className="city-div">
+                <div className="city-photo">
+                    <img src={require('../images/mumbai.png')}/>
                 </div>
                 <h4 className="city-title">Mumbai</h4>
                 <div className="city-margin">
